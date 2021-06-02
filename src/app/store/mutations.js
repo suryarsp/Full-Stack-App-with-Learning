8e4e4ebdea095a28a8da91a3c850450a994958d2
Surya Prakash <rspsuriya777@gmail.com>
2021-06-02 12:50:23 +0530

    Implement saga, redux logger and mutatation for redux saga

--- src/app/store/mutations.js
+++ src/app/store/mutations.js
@@ -1,9 +1,9 @@
 export const REQUEST_TASK_CREATION = "REQUEST_TASK_CREATION";
 export const CREATE_TASK = "CREATE_TASK";
 
-export const requestTaskCreation = (groupId) => ({
+export const requestTaskCreation = (groupID) => ({
     type: REQUEST_TASK_CREATION,
-    groupId
+    groupID
 })
 
 export const createTask = (taskID, groupID, ownerID) => ({
@@ -11,4 +11,4 @@ export const createTask = (taskID, groupID, ownerID) => ({
     taskID,
     groupID,
     ownerID
-})
+});
