6b5d5507015c05989708dfb1c2d9f4302c1be218
Surya Prakash <rspsuriya777@gmail.com>
2021-06-03 11:59:54 +0530

    Install Mongo DB , initalize the DB with script

---
+++ src/server/connect-db.js
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb';
+const URL = 'mongodb://localhost:27017/myorganizer';
+let db = null;
+
+
+export async function connectDB() {
+    if (db) return db;
+    let client = await MongoClient.connect(URL,
+        {
+            useNewUrlParser: true
+        });
+    db = client.db();
+    console.log('DB', db);
+    return db;
+}
